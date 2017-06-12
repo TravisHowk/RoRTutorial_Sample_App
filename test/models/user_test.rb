5219db30eae2a6c37f52f595e2a4c344d647175f
Travis Howk <howkta@hendrix.edu>
2017-06-12 14:52:47 -0500

    Implement password validation and length checks

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,25 +3,30 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example user", email: "user@example.com")
+    @user = User.new(name: "Example user", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
     assert @user.valid?
   end
 
+  # Name Tests #
+
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = "     "
+  test "name should not be too long" do
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-    @user.name = "a" * 51
+  # Email Tests #
+
+  test "email should be present" do
+    @user.email = "     "
     assert_not @user.valid?
   end
 
@@ -60,6 +65,18 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  # Password Tests #
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " *6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a"*5
+    assert_not @user.valid?
   end
 
 end
